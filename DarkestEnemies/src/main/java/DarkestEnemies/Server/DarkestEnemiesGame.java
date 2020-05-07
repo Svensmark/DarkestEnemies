c2dd217049fe7c7991124996654908aaeb2700e5
asgerhs <43413829+asgerhs@users.noreply.github.com>
2020-05-07 16:43:33 +0200

    cleanup and small bug fixes

--- DarkestEnemies/src/main/java/DarkestEnemies/Server/DarkestEnemiesGame.java
+++ DarkestEnemies/src/main/java/DarkestEnemies/Server/DarkestEnemiesGame.java
@@ -30,14 +30,7 @@ public class DarkestEnemiesGame implements ITextGame {
 
         while (true) {
 
-            //Player setup
-            System.out.println("Welcome! We're just getting everyone set up. Please type in the information propmpted to you or wait your turn.\n");
-            for (int i = 0; i < players.length; i++) {
-                players[i].put("Let's start with your name!\nWhat do you wish to be called?:\n");
-                names[i] = players[i].get();
-                Player p = new Player(names[i], 100, 0, 2);
-                playerEntities.add(p);
-            }
+            playerSetup(players, names, playerEntities);
 
             //Start Announcement
             for (int i = 0; i < players.length; i++) {
@@ -45,13 +38,7 @@ public class DarkestEnemiesGame implements ITextGame {
             }
 
             //enemy setup
-            NPC enemy = new NPC("Goblin", 10, 0, 50);
-
-            //introduction to encounter
-            for (int i = 0; i < players.length; i++) {
-                players[i].put("Oh no! You've encountered a goblin!\n");
-                players[i].put("This is your first encounter, should be easy. Just wack a mole him!\n");
-            }
+            NPC enemy = new NPC("Goblin", 10, 0, 5);
 
             //encounter setup
             List<DECharacter> encounter = new ArrayList();
@@ -59,82 +46,9 @@ public class DarkestEnemiesGame implements ITextGame {
                 encounter.add(playerEntities.get(i));
             }
             encounter.add(enemy);
-            
 
-            //Setups bools
-            boolean playerAlive = true;
-            boolean enemyAlive = true;
-
-            //First encounter
-            while (playerAlive == true && enemyAlive == true) {
-
-                //Encounter START
-                for (int i = 0; i < encounter.size(); i++) {
-                    System.out.println(encounter.get(i).getClass());
-                    //If the the character is an NPC
-                    if (encounter.get(i).getClass() == NPC.class) {
-                        
-                        System.out.println("The NPC has taken their turn");
-                        for (int j = 0; j < players.length; j++) {
-                            System.out.println("current hp " + playerEntities.get(j).getHealth());
-                            System.out.println("enemy atkdmg " + enemy.getAttackDmg());
-                            System.out.println(playerEntities.get(j).getHealth() - enemy.getAttackDmg());
-                            playerEntities.get(j).setHealth(playerEntities.get(j).getHealth() - enemy.getAttackDmg());
-                            players[j].put("You've been hit! You now have: " + playerEntities.get(j).getHealth() + " hp left!\n");
-                        }
-                    }
-
-                    //If the character is a player
-                    if (encounter.get(i).getClass() != NPC.class) {
-                        //Creates of list
-                        ArrayList<String> actions = new ArrayList();
-                        //Adds the available actions of the player to the list 
-                        actions.add("Attack");
-                        actions.add("Heal");
-
-                        players[i].clear();
-                        int choice = players[i].select("What do you wish to do?", actions, "");
-                        System.out.println(choice);
-                        switch (choice) {
-                            
-                            //Attack
-                            case 1:
-                                enemy.setHealth(enemy.getHealth() - playerEntities.get(i).getAttackDmg());
-                                players[i].put("Hit! " + enemy.getCharacterName() + " now has " + enemy.getHealth() + " left!\n");
-                                break;
-                            //Heal
-                            case 2:
-                                if (playerEntities.get(i).getHealth() > 90) {
-                                    playerEntities.get(i).setHealth(100);
-                                    System.out.println("Healed some small ammount");
-                                    break;
-                                } else {
-                                    playerEntities.get(i).setHealth(playerEntities.get(i).getHealth());
-                                    System.out.println("Healed for 10!");
-                                    break;
-                                }
-
-                        }
-                    }
-                    
-                    //Checks HP for NPC
-                    if (enemy.getHealth() <= 0) {
-                        System.out.println("Well you've diddley done it! Congrats!");
-                        enemyAlive = false;
-                        break;
-                    }
-
-                    //Checks HP for Players
-                    for (int j = 0; j < players.length; ++j) {
-                        if (playerEntities.get(j).getHealth() <= 0) {
-                            players[j].put("Oh no! You've been killed! Game over!\n");
-                            players[j + 1].put("Oh no!" + playerEntities.get(j).getCharacterName() + " has been killed! You flee in fear. Game over!\n");
-                            playerAlive = false;
-                            break;
-                        }
-                    }                                       
-                }                    
-            }
+            //Encounter
+            firstEncounter(encounter, players, playerEntities, enemy);
 
             //Breaks out of the main loop
             System.out.println("Game Complete!");
@@ -142,6 +56,102 @@ public class DarkestEnemiesGame implements ITextGame {
 
         }
 
+    }
+
+    private void firstEncounter(List<DECharacter> encounter, ITextIO[] players, List<DECharacter> playerEntities, NPC enemy) {
+        //Setups bools
+        boolean playerAlive = true;
+        boolean enemyAlive = true;
+
+        //introduction to encounter
+        for (int i = 0; i < players.length; i++) {
+            players[i].put("Oh no! You've encountered a goblin!\n");
+            players[i].put("This is your first encounter, should be easy. Just wack a mole him!\n");
+        }
+        
+        while (playerAlive == true && enemyAlive == true) {
+
+            //Encounter START
+            for (int i = 0; i < encounter.size(); i++) {
+                
+                System.out.println(encounter.get(i).getClass());
+                //If the the character is an NPC
+                if (encounter.get(i).getClass() == NPC.class) {
+
+                    System.out.println("The NPC has taken their turn");
+                    for (int j = 0; j < players.length; j++) {
+                        System.out.println("current hp " + playerEntities.get(j).getHealth());
+                        System.out.println("enemy atkdmg " + enemy.getAttackDmg());
+                        System.out.println(playerEntities.get(j).getHealth() - enemy.getAttackDmg());
+                        playerEntities.get(j).setHealth(playerEntities.get(j).getHealth() - enemy.getAttackDmg());
+                        players[j].put("You've been hit! You now have: " + playerEntities.get(j).getHealth() + " hp left!\n");
+                    }
+                }
+
+                //If the character is a player
+                if (encounter.get(i).getClass() != NPC.class) {
+                    //Creates of list
+                    ArrayList<String> actions = new ArrayList();
+                    //Adds the available actions of the player to the list
+                    actions.add("Attack");
+                    actions.add("Heal");
+
+                    int choice = players[i].select("What do you wish to do?", actions, "");
+                    System.out.println(choice);
+                    switch (choice) {
+
+                        //Attack
+                        case 1:
+                            enemy.setHealth(enemy.getHealth() - playerEntities.get(i).getAttackDmg());
+                            players[i].clear();
+                            players[i].put("Hit! " + enemy.getCharacterName() + " now has " + enemy.getHealth() + " left!\n");
+                            break;
+                        //Heal
+                        case 2:
+                            if (playerEntities.get(i).getHealth() > 90) {
+                                playerEntities.get(i).setHealth(100);
+                                System.out.println("Healed some small ammount");
+                                break;
+                            } else {
+                                playerEntities.get(i).setHealth(playerEntities.get(i).getHealth());
+                                players[i].clear();
+                                System.out.println("Healed for 10!");
+                                break;
+                            }
+
+                    }
+                    
+                }
+
+                //Checks HP for NPC
+                if (enemy.getHealth() <= 0) {
+                    System.out.println("Well you've diddley done it! Congrats!");
+                    enemyAlive = false;
+                    break;
+                }
+
+                //Checks HP for Players
+                for (int j = 0; j < players.length; ++j) {
+                    if (playerEntities.get(j).getHealth() <= 0) {
+                        players[j].put("Oh no! You've been killed! Game over!\n");
+                        players[j + 1].put("Oh no!" + playerEntities.get(j).getCharacterName() + " has been killed! You flee in fear. Game over!\n");
+                        playerAlive = false;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    private void playerSetup(ITextIO[] players, String[] names, List<DECharacter> playerEntities) {
+        //Player setup
+        System.out.println("Welcome! We're just getting everyone set up. Please type in the information propmpted to you or wait your turn.\n");
+        for (int i = 0; i < players.length; i++) {
+            players[i].put("Let's start with your name!\nWhat do you wish to be called?:\n");
+            names[i] = players[i].get();
+            Player p = new Player(names[i], 100, 0, 2);
+            playerEntities.add(p);
+        }
     }
 
 }
