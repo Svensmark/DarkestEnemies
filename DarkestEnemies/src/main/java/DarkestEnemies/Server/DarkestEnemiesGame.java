db3c37520b86326edb73298355cbe446873155eb
Svensmark <emil.svensmark@gmail.com>
2020-05-12 17:01:52 +0200

    Updated a minor fix

--- DarkestEnemies/src/main/java/DarkestEnemies/Server/DarkestEnemiesGame.java
+++ DarkestEnemies/src/main/java/DarkestEnemies/Server/DarkestEnemiesGame.java
@@ -378,6 +378,7 @@ public class DarkestEnemiesGame implements ITextGame {
             }
         }
         
+        players[i].put("Press enter to continue");
         players[i].get();
         players[i].clear();
         players[i].put("Waiting for players..");
